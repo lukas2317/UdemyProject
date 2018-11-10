e8b7c858e4e8b429b01d5ce7b9feff8e890126f9
Lukas Laudert <lukas.laudert@de.ibm.com>
2018-11-10 21:00:53 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './header/shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
+import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule
